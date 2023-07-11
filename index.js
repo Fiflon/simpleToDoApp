f16a56963431ac686464c69fca448985502bc795
Fiflon <odzera.dotrofera@gmail.com>
2023-07-11 10:18:15 +0200

    dodalem przycisk delete

--- index.js
+++ index.js
@@ -2,7 +2,33 @@ let myTasks = [];
 const btnAddTask = document.getElementById("taskAdder");
 const nameOfTaskPlaceholder = document.getElementById("nameOfNewTask");
 const todoList = document.getElementById("todoList");
-btnAddTask.addEventListener("click", () => {
+
+function addNewTask() {
+  let titleOfTask = nameOfTaskPlaceholder.value.trim();
+  if (titleOfTask == "") {
+    return;
+  }
+  let li = document.createElement("li");
+  let checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+
+  let todoTask = document.createElement("label");
+  todoTask.textContent = titleOfTask;
+
+  let delButton = document.createElement("button");
+  delButton.textContent = "Delete";
+  delButton.addEventListener("click", () => {
+    let li = delButton.parentNode;
+    todoList.removeChild(li);
+  });
+
+  li.appendChild(checkbox);
+  li.appendChild(todoTask);
+  li.appendChild(delButton);
+  todoList.appendChild(li);
+}
+
+/* btnAddTask.addEventListener("click", () => {
   let titleOfTask = nameOfTaskPlaceholder.value.trim();
   if (titleOfTask == "") {
   } else {
@@ -17,4 +43,4 @@ btnAddTask.addEventListener("click", () => {
     li.appendChild(todoTask);
     todoList.appendChild(li);
   }
-});
+}); */
